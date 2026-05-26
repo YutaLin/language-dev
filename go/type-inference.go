27b5a32f295ac18c43d2d0df8a5697255a17859f
Bolin Lin <linbolin1230@gmail.com>
2026-05-26 14:29:53 -0400

    feat: basic types, constant, type inference, type conversion, zero

---
+++ go/type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42
+	x := 3.142
+	g := 0.867 + 0.5i
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("v is of type %T\n", x)
+	fmt.Printf("v is of type %T\n", g)
+}
