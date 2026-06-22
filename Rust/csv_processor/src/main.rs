c6e3af0a9ed17252fb8fd1c39acd2776a2270321
Bolin Lin <linbolin1230@gmail.com>
2026-06-22 17:54:32 -0400

    csv_process:feat: custom reader with iterator

--- Rust/csv_processor/src/main.rs
+++ Rust/csv_processor/src/main.rs
@@ -2,18 +2,45 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use anyhow::Result;
 
+pub struct CsvReader {
+    reader: BufReader<File>,
+}
+
+impl CsvReader {
+    pub fn open(path: &str) -> Result<Self> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        Ok(CsvReader{ reader })
+    }
+}
+
+impl Iterator for CsvReader {
+    type Item = Result<Vec<String>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut line = String::new();
+        match self.reader.read_line(&mut line) {
+            Ok(0) => None,
+            Ok(_) => {
+                let trimmed = line.trim_end();
+                let fields: Vec<String> = trimmed
+                    .split(",")
+                    .map(|s| s.to_string())
+                    .collect();
+                Some(Ok(fields))
+            }
+            Err(e) => Some(Err(e.into())),
+        }
+    }
+}
+
 fn main() -> Result<()> {
-    let file = File::open("sample.csv")?;
-    let reader = BufReader::new(file);
+    let csv = CsvReader::open("sample.csv")?;
 
-    let mut all_first_fields: Vec<String> = Vec::new();
-
-    for line in reader.lines() {
-        let line: String = line?;
-        let fields: Vec<&str> = line.split(',').collect();
-        all_first_fields.push(fields[0].to_string());
+    for row in csv {
+        let row = row?;
+        println!("{:?}", row);
     }
 
-    println!("{:?}", all_first_fields);
     Ok(())
 }
